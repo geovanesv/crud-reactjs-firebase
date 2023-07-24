7650fddb3a9fb1d6f4478a3ec7cf24a47dbd89ca
Geovane Araujo <49567926+geovanesv@users.noreply.github.com>
2023-07-24 20:31:30 -0300

    Update firebaseConnection.jsx

--- src/services/firebaseConnection.jsx
+++ src/services/firebaseConnection.jsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBV1Fu-ilK6ya70Pyb1l7SVVl25aeZfovU",
-  authDomain: "crud-reactjs-pj2.firebaseapp.com",
-  projectId: "crud-reactjs-pj2",
-  storageBucket: "crud-reactjs-pj2.appspot.com",
-  messagingSenderId: "1070618035990",
-  appId: "1:1070618035990:web:7db923c8be5c8f1cb9b237"
+  apiKey: "#",
+  authDomain: "#",
+  projectId: "#",
+  storageBucket: "#",
+  messagingSenderId: "#",
+  appId: "#"
 };
 
 
